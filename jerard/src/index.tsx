1bd0d6c93bfb869e5acb63f6f61983dd9ce8e04c
Ansat Jartybayev <ansatjr@gmail.com>
2022-01-18 18:05:31 +0600

    chore(jerard): add store provider

--- jerard/src/index.tsx
+++ jerard/src/index.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import {render} from "react-dom";
+import {Provider} from "react-redux";
 
 import {App} from "@app/client";
+import {store} from "@shared/lib/store";
 
 const root = document.getElementById("root");
 
 render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   root,
 );

94ff11dd98f42f4b6555916e88534b83bf2ec928
Ansat Jartybayev <ansatjr@gmail.com>
2022-01-18 15:14:53 +0600

    chore(jerard): update webpack config to ts

---
+++ jerard/webpack.config.ts
@@ -0,0 +1,94 @@
+import * as path from "path";
+import * as webpack from "webpack";
+import * as HTMLWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
+import "webpack-dev-server";
+
+interface ConfigurationProps {
+  env: "production" | "development";
+}
+
+export default ({env}: ConfigurationProps): webpack.Configuration => {
+  const isDev = env === "development";
+  const isProd = env === "production";
+
+  return {
+    mode: env,
+    entry: path.join(__dirname, "src/index.tsx"),
+    output: {
+      path: path.join(__dirname, "build"),
+      filename: "[contenthash].bundle.js",
+      clean: true,
+    },
+    optimization: {
+      minimize: isProd,
+      runtimeChunk: true,
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+    resolve: {
+      extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: path.join(__dirname, "tsconfig.json"),
+        }) as unknown as webpack.ResolvePluginInstance,
+      ],
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        template: path.join(__dirname, "public/index.html"),
+      }),
+      new ForkTsCheckerWebpackPlugin({
+        typescript: {
+          configFile: path.join(__dirname, "tsconfig.json"),
+        },
+        async: false,
+        eslint: {
+          enabled: true,
+          files: "./{src,typings}/**/*.{ts,tsx,js,jsx,json}",
+        },
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                minified: isProd,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    devtool: isDev && "eval",
+    devServer: {
+      hot: true,
+      port: 3000,
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, "public"),
+        publicPath: "/public",
+      },
+    },
+    stats: "errors-warnings",
+  };
+};

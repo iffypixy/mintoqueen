ea6c53398485f2826b57d22e515714f7cfa51141
Ansat Jartybayev <ansatjr@gmail.com>
2022-01-19 10:42:27 +0600

    feat(jerard): add auth-model utils

--- jerard/src/features/auth/model/reducer.ts
+++ jerard/src/features/auth/model/reducer.ts
@@ -1,12 +1,18 @@
 import {createReducer} from "@reduxjs/toolkit";
 
+import {Credentials} from "@shared/api/auth";
+
 export interface AuthState {
   isAuthenticated: boolean;
+  credentials: Credentials | null;
+  isCredentialsFetchPending: boolean;
 }
 
 export const reducer = createReducer<AuthState>(
   {
     isAuthenticated: false,
+    credentials: null,
+    isCredentialsFetchPending: false,
   },
   {},
 );
--- jerard/src/features/auth/model/selectors.ts
+++ jerard/src/features/auth/model/selectors.ts
@@ -4,3 +4,8 @@ const authState = (state: RootStore) => state.auth;
 
 export const isAuthenticated = (state: RootStore) =>
   authState(state).isAuthenticated;
+
+export const credentials = (state: RootStore) => authState(state).credentials;
+
+export const isCredentialsFetchPending = (state: RootStore) =>
+  authState(state).isCredentialsFetchPending;

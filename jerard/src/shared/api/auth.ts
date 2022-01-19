788b36621dfed0c313580ac2b69de00c35788c42
Ansat Jartybayev <ansatjr@gmail.com>
2022-01-19 16:47:56 +0600

    feat(jerard): add auth-api setup

--- jerard/src/shared/api/auth.ts
+++ jerard/src/shared/api/auth.ts
@@ -1,6 +1,21 @@
+import {request} from "@shared/lib/request";
+
 export interface Credentials {
   id: string;
   username: string;
   email: string;
   avatar: string;
 }
+
+export interface GetCredentialsResponse {
+  credentials: Credentials;
+}
+
+const getCredentials = () =>
+  request<GetCredentialsResponse>({
+    url: "/api/auth/credentials",
+  });
+
+export const authApi = {
+  getCredentials,
+};

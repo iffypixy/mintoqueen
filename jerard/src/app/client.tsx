8c9ae4e63967f3f1ccb6979d309927e8eb76b49c
Ansat Jartybayev <ansatjr@gmail.com>
2022-01-28 01:40:09 +0600

    refactor(jerard): update client

--- jerard/src/app/client.tsx
+++ jerard/src/app/client.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 
 import {CredentialsObtainer} from "@features/auth";
+import {Routes} from "@pages/routes";
 
-export const App: React.FC = () => <CredentialsObtainer />;
+export const App: React.FC = () => (
+  <CredentialsObtainer>
+    <Routes />
+  </CredentialsObtainer>
+);

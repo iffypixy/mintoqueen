fa6af9a43be3d6a8876f5975ad918895f3e1a371
Ansat Jartybayev <ansatjr@gmail.com>
2022-01-27 14:22:11 +0600

    chore(jerard): update root-reducer

--- jerard/src/shared/lib/store/root-reducer.ts
+++ jerard/src/shared/lib/store/root-reducer.ts
@@ -1,7 +1,9 @@
 import {combineReducers} from "redux";
 
 import {authReducer} from "@features/auth";
+import {themingReducer} from "@shared/lib/theming";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
+  theming: themingReducer,
 });

0149bb100a8e01ed648f6618509562c646f18d7e
Ansat Jartybayev <ansatjr@gmail.com>
2022-01-28 22:28:31 +0600

    feat(jerard): init i18n

--- jerard/src/index.tsx
+++ jerard/src/index.tsx
@@ -6,6 +6,8 @@ import {BrowserRouter} from "react-router-dom";
 import {App} from "@app/client";
 import {store} from "@shared/lib/store";
 
+import "@shared/lib/i18n";
+
 const root = document.getElementById("root");
 
 render(

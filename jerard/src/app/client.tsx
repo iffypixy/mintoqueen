5a965e7ef7daed369c7b058a8210660548dee2f3
Ansat Jartybayev <ansatjr@gmail.com>
2022-01-29 13:50:07 +0600

    feat(jerard): init global-styles

--- jerard/src/app/client.tsx
+++ jerard/src/app/client.tsx
@@ -2,9 +2,15 @@ import * as React from "react";
 
 import {CredentialsObtainer} from "@features/auth";
 import {Routes} from "@pages/routes";
+import {GlobalStyles} from "./global-styles";
+
+const globalStyles = <GlobalStyles />;
 
 export const App: React.FC = () => (
-  <CredentialsObtainer>
-    <Routes />
-  </CredentialsObtainer>
+  <>
+    {globalStyles}
+    <CredentialsObtainer>
+      <Routes />
+    </CredentialsObtainer>
+  </>
 );

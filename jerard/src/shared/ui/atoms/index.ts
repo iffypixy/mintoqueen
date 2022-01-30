d0fe98671b16d90146df70eb8ba2683350b9a42f
Ansat Jartybayev <ansatjr@gmail.com>
2022-01-30 18:19:40 +0600

    chore(jerard): export ui-atoms

--- jerard/src/shared/ui/atoms/index.ts
+++ jerard/src/shared/ui/atoms/index.ts
@@ -1,1 +1,6 @@
 export {H1, H2, H3, H4, H5, H6} from "./heading";
+export {OutlinedInput} from "./input";
+export {Link} from "./link";
+export {Button} from "./button";
+export {Text} from "./text";
+export {Icon} from "./icon";

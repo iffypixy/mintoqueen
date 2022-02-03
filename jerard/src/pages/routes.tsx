333e44830821e71cc2aa4920246ff7edfaccd2ea
Ansat Jartybayev <ansatjr@gmail.com>
2022-02-03 23:20:07 +0600

    feat(gerard): add route-paths

--- jerard/src/pages/routes.tsx
+++ jerard/src/pages/routes.tsx
@@ -1,3 +1,12 @@
 import * as React from "react";
+import {Routes as Switch, Route} from "react-router-dom";
 
-export const Routes: React.FC = () => null;
+import {RegisterPage} from "./register";
+import {LoginPage} from "./login";
+
+export const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/register" element={<RegisterPage />} />
+    <Route path="/login" element={<LoginPage />} />
+  </Switch>
+);

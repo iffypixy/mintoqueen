482e946fb95223e1dc8b7a2899bb246cd5c49ad8
Ansat Jartybayev <ansatjr@gmail.com>
2022-02-04 22:50:14 +0600

    refactor(gerard): update input

--- gerard/src/shared/ui/atoms/input.tsx
+++ gerard/src/shared/ui/atoms/input.tsx
@@ -16,17 +16,19 @@ forward.displayName = "OutlinedInput";
 
 export const OutlinedInput = React.forwardRef(forward);
 
+const fieldset = ".MuiOutlinedInput-notchedOutline";
+
 const OutlinedInputNative = styled(TextField)`
   height: 8rem;
 
   & > label {
     color: ${({theme}) => opacify(theme.palette.text.primary, 0.6)};
     font-size: 1.6rem;
-    transform: translate(2rem, 2rem) scale(1);
+    transform: translate(2rem, 2.1rem) scale(1);
 
     &.MuiInputLabel-shrink {
       color: ${({theme}) => theme.palette.text.primary};
-      transform: translate(2.3rem, 1rem) scale(0.85);
+      transform: translate(2rem, 1rem) scale(0.85);
     }
 
     &.Mui-error {
@@ -40,9 +42,16 @@ const OutlinedInputNative = styled(TextField)`
   }
 
   & > div {
+    &:hover {
+      & > ${fieldset} {
+        border-color: ${({theme}) => theme.palette.primary.main};
+      }
+    }
+
     &.Mui-focused {
-      & > input {
-        border: 2px solid ${({theme}) => theme.palette.primary.main};
+      & > ${fieldset} {
+        box-shadow: 0 0 0 2px ${({theme}) => theme.palette.primary.main};
+        border-width: 1px;
       }
     }
 
@@ -50,20 +59,37 @@ const OutlinedInputNative = styled(TextField)`
       & > input {
         background-color: ${({theme}) =>
           opacify(theme.palette.error.main, 0.15)};
-        border: 2px solid ${({theme}) => theme.palette.error.main};
+      }
+
+      & > ${fieldset} {
+        border-color: ${({theme}) => theme.palette.error.main};
+        border-width: 1px;
+      }
+
+      &.Mui-focused {
+        & > ${fieldset} {
+          box-shadow: 0 0 0 2px ${({theme}) => theme.palette.error.main};
+          border-width: 1px;
+        }
       }
     }
 
     & > input {
       background-color: ${({theme}) =>
         opacify(theme.palette.primary.main, 0.15)};
-      border: 1px solid ${({theme}) => theme.palette.divider};
       border-radius: ${({theme}) => theme.shape.borderRadius};
       padding: 3rem 2rem 1rem;
     }
 
-    & > fieldset {
-      border: none;
+    & > ${fieldset} {
+      border-width: 1px;
+      border-style: solid;
+      border-color: ${({theme}) => theme.palette.divider};
+      border-radius: ${({theme}) => theme.shape.borderRadius};
+      transition-property: box-shadow, border;
+      transition-timing-function: linear;
+      transition-duration: 0.2s;
+      top: 0;
 
       & > legend {
         display: none;
@@ -74,6 +100,6 @@ const OutlinedInputNative = styled(TextField)`
   & > p {
     font-size: 1.4rem;
     margin-left: 0.5rem;
-    margin-top: 0;
+    margin-top: 0.25rem;
   }
 `;

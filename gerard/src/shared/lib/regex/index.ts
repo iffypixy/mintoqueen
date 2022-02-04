1d897ffb79a3971c0addf85d126d82346cbe97a8
Ansat Jartybayev <ansatjr@gmail.com>
2022-02-04 22:50:41 +0600

    feat(gerard): add username-regex

--- gerard/src/shared/lib/regex/index.ts
+++ gerard/src/shared/lib/regex/index.ts
@@ -1,6 +1,6 @@
 const email =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const alphanumeric = /^\w+$/;
+const username = /^(?=.{5,20}$)(?![_0-9])(?!.*[_]{2})[a-zA-Z0-9_]+(?<![_])$/;
 
-export const regex = {email, alphanumeric};
+export const regex = {email, username};
